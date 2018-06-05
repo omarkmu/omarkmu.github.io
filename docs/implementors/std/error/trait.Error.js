5c12bd83a6732d84770de0b0603a4e479c109d38
Omar M <omarkmu@gmail.com>
2018-06-05 19:46:36 -0400

    🎉 Initial commit

---
+++ docs/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["winconsole"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"winconsole/errors/struct.ArgumentError.html\" title=\"struct winconsole::errors::ArgumentError\">ArgumentError</a>",synthetic:false,types:["winconsole::errors::argument_error::ArgumentError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"winconsole/errors/struct.InvalidHandleError.html\" title=\"struct winconsole::errors::InvalidHandleError\">InvalidHandleError</a>",synthetic:false,types:["winconsole::errors::invalid_handle_error::InvalidHandleError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"winconsole/errors/enum.WinError.html\" title=\"enum winconsole::errors::WinError\">WinError</a>",synthetic:false,types:["winconsole::errors::win_error::WinError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/winconsole/errors/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["WinError","Contains wrapped error types."]],"struct":[["ArgumentError","Describes an error related to an argument."],["InvalidHandleError","Describes an error related to an invalid handle."]],"type":[["WinResult","Represents a result which contains either a returned value or a WinError."]]});
